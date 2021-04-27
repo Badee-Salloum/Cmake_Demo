f397260b65445a47292b74fe6201a4eca9d9a305
Michael Jabbour <m.jabbour@lit-co.net>
2021-04-27 11:03:15 +0300

    02. Add image resource

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
 
-    QLabel label("Hello world!");
+    QLabel label;
+    label.setPixmap(QPixmap(":/images/Qt.png"));
 
     label.resize(640, 480);
-    label.setAlignment(Qt::AlignCenter);
-    label.setStyleSheet("font: 50px; color: brown;");
+    label.setScaledContents(true);
     label.show();
 
     return app.exec();
