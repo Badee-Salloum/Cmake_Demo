761c9c53a10059988c742ae0017cae135f478b63
Michael Jabbour <m.jabbour@lit-co.net>
2021-05-04 09:50:13 +0300

    03. Add AddLib library

---
+++ AddLib/add.cpp
@@ -0,0 +1,6 @@
+#include "add.h"
+
+
+int Adder::add(int a, int b) {
+    return a + b;
+}
--- main.cpp
+++ main.cpp
@@ -1,15 +1,27 @@
 #include <QtWidgets>
 
+#include "add.h"
+
 
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
 
+    // image label to display Qt.png from resources
     QLabel label;
     label.setPixmap(QPixmap(":/images/Qt.png"));
 
     label.resize(640, 480);
     label.setScaledContents(true);
     label.show();
+
+    // text label to diplay 1 + 2 calculated using AddLib
+    QLabel textLabel;
+    Adder adder;
+    textLabel.setText(QString::number(adder.add(1, 2)));
+
+    textLabel.setAlignment(Qt::AlignCenter);
+    textLabel.setStyleSheet("font: 50px; color: brown;");
+    textLabel.show();
 
     return app.exec();
 }
